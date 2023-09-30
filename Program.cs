e3f10c8582859f912ca0562c83e9fc055921b6cb
Nerses Hepoyan <nerseshepoyan22@gmail.com>
2023-09-30 07:51:56 -0800

    Thread example

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿
+using ThreadsExample;
+
+Thread thread = new(new ThreadStart(ThreadFuncshons.ThreadExample));
+
+Console.WriteLine("Id for the primary thread {0} \n", Thread.CurrentThread.GetHashCode());
+Console.WriteLine("Start for the primary thread..");
+
+thread.Start();
+
+Console.WriteLine(Thread.CurrentThread.GetHashCode());
+
+
+Console.ForegroundColor = ConsoleColor.Gray;
+
+for (int i = 0; i < 10; ++i)
+{
+    Thread.Sleep(25);
+    Console.Write("-");
+}
+Console.ForegroundColor = ConsoleColor.Magenta;
+Console.WriteLine("\nPrimary finished");
+Console.ReadLine();
---
+++ ThreadFuncshons.cs
@@ -0,0 +1,21 @@
+﻿namespace ThreadsExample
+{
+    internal class ThreadFuncshons
+    {
+        public static void ThreadExample()
+        {
+            Console.WriteLine("Start the secondery Thread");
+            Console.WriteLine("Id for secondery Thread: {0}", Environment.CurrentManagedThreadId);
+            Console.ForegroundColor = ConsoleColor.Yellow;
+
+            for (int i = 0; i < 10; ++i) 
+            {
+                Thread.Sleep(25);
+                Console.Write(".");
+            }
+            Console.ForegroundColor= ConsoleColor.Green;
+            Console.WriteLine("Secondery thread finished");
+
+        }
+    }
+}
